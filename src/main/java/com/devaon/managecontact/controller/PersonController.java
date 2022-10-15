73016abe7533bb8c8d6c04ccf8c15f2aa6bf0198
dogeonK <102578327+dogeonK@users.noreply.github.com>
2022-10-15 14:53:33 +0900

    [UPDATE] PersonController

--- src/main/java/com/devaon/managecontact/controller/PersonController.java
+++ src/main/java/com/devaon/managecontact/controller/PersonController.java
@@ -40,13 +40,4 @@ public class PersonController {
         personService.modify(id, personDto);
     }
 
-    @PatchMapping("/{id}")
-    public void modifyPerson(@PathVariable Long id, String name) {
-        personService.modify(id, name);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deletePerson(@PathVariable Long id){
-        personService.delete(id);
-    }
 }
